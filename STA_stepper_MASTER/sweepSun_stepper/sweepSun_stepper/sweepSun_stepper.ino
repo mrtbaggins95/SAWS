a0631d2859c1c739b17d3d7e4428db8c5c94124b
GamBondoc <gambondoc@gmail.com>
2017-10-17 13:48:11 -0700

    Initiated sweepSun_stepper

---
+++ STA_stepper_MASTER/sweepSun_stepper/sweepSun_stepper/sweepSun_stepper.ino
@@ -0,0 +1,14 @@
+#include <Stepper.h>
+
+Stepper AzStepper = Stepper(200,1,2);
+Stepper AlStepper = Stepper(200,3,4);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
