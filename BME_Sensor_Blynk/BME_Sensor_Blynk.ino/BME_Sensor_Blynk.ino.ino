d862b4c3d6af24b3f8e757f704d29a84155537c5
mrtbaggins95 <sakib.karim95@gmail.com>
2017-11-30 18:41:26 -0800

    Working Bluetooth
    1. Upload code
    2. Connect using the bluetooth widget
    3. make sure you use the auth code

--- BME_Sensor_Blynk/BME_Sensor_Blynk.ino/BME_Sensor_Blynk.ino.ino
+++ BME_Sensor_Blynk/BME_Sensor_Blynk.ino/BME_Sensor_Blynk.ino.ino
@@ -1,60 +1,37 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Social networks:            http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  For this example you need BLEPeripheral library
-    from http://librarymanager/all#BLEPeripheral
-    or https://github.com/sandeepmistry/arduino-BLEPeripheral
-
-  Warning: Bluetooth support is in beta!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
-
 
 #define BLYNK_PRINT Serial
 #define BLYNK_USE_DIRECT_CONNECT
-#include <Adafruit_BME280.h>
 #include <BlynkSimpleSerialBLE.h>
 #include <BLEPeripheral.h>
 #include "BLESerial.h"
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_BME280.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken";
+char auth[] = " db551ad1ea9b465592128b64f96b294a";
 
 // define pins (varies per shield/board)
+//BLUETOOTH SHIT
 #define BLE_REQ   10
 #define BLE_RDY   2
 #define BLE_RST   9
 
- // I2C
+//BME SHIT
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 8
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
 // create ble serial instance, see pinouts above
 BLESerial SerialBLE(BLE_REQ, BLE_RDY, BLE_RST);
-
-
+Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 BLYNK_WRITE(V1)
 {
@@ -102,15 +79,6 @@ void loop()
   SerialBLE.poll();
 
   if (SerialBLE) {    // If BLE is connected...
-
-  
-
-
-
-
-
-
-    
     Blynk.run();
   }
 }
