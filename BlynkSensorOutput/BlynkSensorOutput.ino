f5b9fcc4d6d8c153dc82d0088d024339421ace6c
stealthwings <jordyhsu@gmail.com>
2017-12-16 18:31:44 -0800

    testing code 12/16/17 18:31
    ignore the changes

--- BlynkSensorOutput/BlynkSensorOutput.ino
+++ BlynkSensorOutput/BlynkSensorOutput.ino
@@ -46,7 +46,7 @@
 
 
 //Auth token to connect to the mobile app
-char auth[] = "8963a3ec5af844e5b97898f6a0e01916";
+char auth[] = "8963a3ec5af844e5b97898f6a0e01916";//ipad blynk
 
 
 //define pins for BLE for Blynk
@@ -99,40 +99,50 @@ void setup()
 void sendBMEData()
 {
   //The the bme.readExample() reads a desired measurement
-  float bmeTemperature = bme.readTemperature();
-  float bmePressure = bme.readPressure()/100.0F;
-  float bmeHumidity = bme.readHumidity();
+  int bmeTemperature = bme.readTemperature();
+  int bmePressure = bme.readPressure()/100.0F;
+  int bmeHumidity = bme.readHumidity();
   //This writes the data to a virtual pin. 
   //Virtual Pins in Blynk act similar to regular pins, 
   //by wrtiing data to virtual pins we are able to read data on the app.
-  Blynk.virtualWrite(V1,bmeTemperature);
-  Blynk.virtualWrite(V2,bmePressure);
-  Blynk.virtualWrite(V3,bmeHumidity);
+  Blynk.virtualWrite(V1,UVindex_val());
+  Blynk.virtualWrite(V2,bmeHumidity);
+  Blynk.virtualWrite(V3,bmePressure);
+  Blynk.virtualWrite(V4,bmeTemperature);
 }
 //The function will return a string based on the values the UV sensor is reading.
-String UVindex_val()
+uint16_t UVindex_val()
 {
   uint16_t reading = uv.readUV();
+  //String output;
+  /*
   if (reading >= 2055)
   {
-    return "Extreme";
+    output =  "Extreme";
+    return output;
   }
   else if ( reading >= 1494)
   {
-    return "Very High";
+    output = "Very High";
+    return output;
   }
   else if ( reading >= 1121)
   {
-    return "High";
+    output = "High";
+    return output;
   }
   else if (reading >= 561)
   {
-    return "Moderate";
+    output = "Moderate";
+    return output;
   }
   else
   {
-    return "Low";
+    output = "Low";
+    return output;
   }
+  */
+  return reading; 
 }
 //Sends Sensor data
 void sendSensor()
